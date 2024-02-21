abb8e51e32d6010b5fa45aca2cbf02a751d87069
Patrick Murphy <patrickmurphydev@gmail.com>
2024-02-21 04:12:59 -0800

    update about page layout  (force)

--- src/app/[locale]/(unauth)/about/page.tsx
+++ src/app/[locale]/(unauth)/about/page.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable no-plusplus */
+import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
+
+import { AppConfig } from '@/utils/AppConfig';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
@@ -16,25 +20,78 @@ export async function generateMetadata(props: { params: { locale: string } }) {
 export default function About() {
   const t = useTranslations('About');
 
+  let eltCount = 0;
   return (
     <div>
-      <h1>About Warrior Quick Stop</h1>
-      <p>{t('about_paragraph')}</p>
-
-      <h2>Contact Information</h2>
-      <div>
-        <p>
-          <span>Icon</span> Phone:{' '}
-        </p>
-        <p>Address: </p>
-        <p>Email: </p>
+      <div
+        className="relative w-full overflow-hidden rounded-md shadow-sm"
+        style={{ height: 140 }}
+      >
+        <Image
+          src={AppConfig.aboutPageDetails.imageSrcURL}
+          alt="bmw outside"
+          fill
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center -790px',
+          }}
+        />
       </div>
-
-      <h3>Managment & Ownership</h3>
-      <div>
-        <h4>Victor Singh (phone/email)</h4>
-        <h4>Harsh Singh (phone/email)</h4>
-        <h4>Gary Singh (phone/email)</h4>
+      <div className="w-full">
+        <div className="relative m-2 rounded border border-solid border-gray-300 p-1 shadow-md">
+          <h1
+            className="border-b-4 border-solid pl-2"
+            style={{ borderColor: AppConfig.colors[1] }}
+          >
+            About Warrior Quick Stop
+          </h1>
+          <p className="p-2 text-sm" style={{ marginBottom: 1, marginTop: 1 }}>
+            {t('about_paragraph')}
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
+        <div className="relative inline-block align-top">
+          <div className="relative m-2 rounded border border-solid border-gray-300 p-1 shadow-md">
+            <h2
+              className="border-b-2 border-solid pl-2"
+              style={{ borderColor: AppConfig.colors[1] }}
+            >
+              Contact Information
+            </h2>
+            <div className="pl-3 text-xs">
+              <p style={{ marginTop: 2, marginBottom: 0 }}>
+                <span className="text-lg">☎️</span> Phone:
+                {` ${AppConfig.aboutPageDetails.phoneNumber}`}
+              </p>
+              <p style={{ marginTop: 0, marginBottom: 0 }}>
+                <span className="text-lg">📍</span> Address:
+                {` ${AppConfig.aboutPageDetails.address.line1}, ${AppConfig.aboutPageDetails.address.city}, ${AppConfig.aboutPageDetails.address.state} ${AppConfig.aboutPageDetails.address.zipcode}`}
+              </p>
+              <p style={{ marginTop: 0, marginBottom: 0 }}>
+                <span className="text-lg">📧</span> Email:
+                {` ${AppConfig.aboutPageDetails.email}`}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="inline-block align-top">
+          <div className="relative m-2 rounded border border-solid border-gray-300 p-1 shadow-md">
+            <h3
+              className="border-b-2 border-solid pl-2"
+              style={{ borderColor: AppConfig.colors[1] }}
+            >
+              Managment & Ownership
+            </h3>
+            <div className="text-sm">
+              <ul className="pl-3" style={{ marginTop: 5, marginBottom: 3 }}>
+                {Array.from(AppConfig.aboutPageDetails.staff).map((elt) => {
+                  return <li key={eltCount++}>{elt}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
